3324e17460379bee534061152c0f8e55e6a8ffa9
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-18 14:09:49 -0700

    Footer styles

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,9 +4,16 @@
  * @returns {string} - The footer template
  */
 const createFooterTemplate = () => {
-	return `
+  const currentYear = new Date().getFullYear();
+
+  return `
     <footer>
-      <div id="menu-opener" class="mobile__menu_button"></div>
+      <div class="footer-container">
+        <div id="menu-opener" class="mobile__menu_button"></div>
+        <div class="footer-wrapper">
+          <p>© ${currentYear} Alexander Khoroshilov</p>
+        </div>
+      </div>
     </footer>
   `;
 };
