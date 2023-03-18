19ae641e5202091adb682f429d924ce4cdb1e194
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-18 01:13:05 -0700

    markup works

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ import { getHomeContent } from './app/contentLoader.js';
 import styles from './styles/styles.scss';
 
 (async () => {
-	await createPageStructure();
+	const additionalContent = `
+    <div class="first-screen">
+      <div class="content-wrapper">
+        <div class="text-wrapper">
+          <h1>Alexander Khoroshilov</h1>
+          <h2>Frontend Web Developer</h2>
+          <p>What makes me stand out is my expertise in designing and updating websites and online applications by utilizing latest programming languages and designing tools. Throughout my career, I have been instrumental in architecting and coding functional and aesthetic components of client websites, producing webpage layouts, editing website content, and making adjustments based on client feedback.</p>
+        </div>
+        <img src="images/profile_photo.webp" alt="Your Photo" />
+      </div>
+      <div class="tech-stack">
+        <span class="tech-stack-title">Tech Stack |</span>
+        <hr />
+        <div class="icon-wrapper">
+          <i class="fab fa-js-square icon"></i>
+          <i class="fab fa-html5 icon"></i>
+          <i class="fab fa-css3-alt icon"></i>
+          <i class="fab fa-sass icon"></i>
+          <i class="fab fa-react icon"></i>
+          <i class="fab fa-php icon"></i>
+        </div>
+      </div>
+      <div class="scroll-wrapper"><a class="scroll-down" href="#page">&#x2193;</a></div>
+    </div>
+  `;
+
+	await createPageStructure(additionalContent);
 
 	const wrapper = getPageWrapper();
 	const content = await getHomeContent();
-	wrapper.innerHTML = content;
+	wrapper.insertAdjacentHTML('beforeend', content);
 })();
