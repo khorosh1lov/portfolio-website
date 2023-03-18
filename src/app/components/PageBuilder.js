f09f778bc779ac86d4c79ecab79106ac027d6f7e
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-17 23:58:29 -0700

    PageBuilder implementation

---
+++ src/app/components/PageBuilder.js
@@ -0,0 +1,25 @@
+import createFooterTemplate from './Footer.js';
+import createHeaderTemplate from './Header.js';
+import { initMenu } from './Menu.js';
+
+export async function createPageStructure() {
+	const headerTemplate = await createHeaderTemplate();
+	const footerTemplate = createFooterTemplate();
+
+	document.body.innerHTML = `
+    ${headerTemplate}
+    <main id="page">
+      <div class="page-wrapper">
+      </div>
+    </main>
+    ${footerTemplate}
+  `;
+
+	initMenu();
+}
+
+export function getPageWrapper() {
+	const main = document.querySelector('#page');
+	const wrapper = main.querySelector('.page-wrapper');
+	return wrapper;
+}
