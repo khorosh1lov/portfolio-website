150b2c490699bf058acb7d8b0727a314bb5b8e55
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-19 01:13:28 -0700

    class Router was added

---
+++ src/app/Router.js
@@ -0,0 +1,38 @@
+class Router {
+	constructor(routes) {
+		this.routes = routes;
+		this._loadInitialRoute();
+	}
+
+	loadRoute(path) {
+		const matchedRoute = this._matchUrlToRoute(path);
+		if (!matchedRoute) {
+			console.error(`No route found for path: ${path}`);
+			return;
+		}
+
+		matchedRoute.action();
+	}
+
+	_matchUrlToRoute(urlPath) {
+		const matchedRoute = this.routes.find((route) => {
+			const routePathSegments = route.path.split('/').slice(1);
+			const urlPathSegments = urlPath.split('/').slice(1);
+
+			if (routePathSegments.length !== urlPathSegments.length) {
+				return false;
+			}
+
+			return routePathSegments.every((routePathSegment, i) => {
+				return routePathSegment === urlPathSegments[i] || routePathSegment[0] === ':';
+			});
+		});
+
+		return matchedRoute;
+	}
+
+	_loadInitialRoute() {
+		const path = window.location.pathname;
+		this.loadRoute(path);
+	}
+}
