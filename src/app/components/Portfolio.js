e6cf81f807f4c55d0f4fdcffe0a9feabfd659d00
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-03-19 15:22:06 -0700

    Portfolio styles

--- src/app/components/Portfolio.js
+++ src/app/components/Portfolio.js
@@ -4,18 +4,24 @@ export function portfolioTemplate(item) {
 	const thumbnailUrl = item._embedded['wp:featuredmedia'] ? item._embedded['wp:featuredmedia'][0].source_url : '';
 
 	return `
-    <article class="post">
-        <div class="card"> 
-            <a href="https://${item.slug}${SUBDOMAIN_URL}" data-slug="${item.slug}" class="post-link" target="_blank"> 
-                <div class="card-image">
-                    <img src="${thumbnailUrl}" alt="${item.title.rendered}">
-                </div> 
-                <div class="card-content"> 
-                    <h2>${item.title.rendered}</h2> 
-                    <i>${item.content.rendered.split('.')[0]}.</i> 
-                </div> 
-            </a> 
-        </div> 
+    <article class="project">
+        <div class="card">
+            <div class="card-image">
+                <img src="${thumbnailUrl}" alt="${item.title.rendered}">
+            </div>
+            <div class="card-content">
+                <h2>${item.title.rendered}</h2>
+                <p>${item.content.rendered.split('.')[0]}.</p>
+                <div class="links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                        Code <i class="fa-brands fa-github link-icon"></i>
+                    </a>
+                    <a target="_blank" href="https://${item.slug}${SUBDOMAIN_URL}" data-slug="${item.slug}" rel="noreferrer">
+                        Live Demo <i class="fa-solid fa-arrow-up-right-from-square link-icon"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </article>
     `;
 }
